455e4e2dbb5eadb0cf2bf1da59374de623501ed3
Angie Cohen <angiedev2001@gmail.com>
2024-04-22 17:53:34 -0300

    add: se agregaron los primeros estilos para el Navbar, las listas, se agrego la imagen del menu de hamburguesa

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Header } from "./components/Header"
 function App() {
 
   return (
-    <>
+    <main className="px-4 pt-6">
      <Header />
-    </>
+    </main>
   )
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import { Navbar } from './Navbar';
+
 import logo from '../assets/images/logo.svg'
 
 export const Header = () => {
   return (
-    <header>
+    <header className=' flex place-content-between items-center'>
       <img src={logo} alt="Logo" />
+      <Navbar /> 
     </header>
   );
 };
