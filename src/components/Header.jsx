46c6628947b059b63d59625ba0a5dd74916ee925
Angie Cohen <angiedev2001@gmail.com>
2024-04-22 18:54:46 -0300

    add: creacion de los componentes NewContainer y NewArticle con sus respectivos estilos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/images/logo.svg'
 
 export const Header = () => {
   return (
-    <header className=' flex place-content-between items-center'>
+    <header className=' flex place-content-between items-center mb-8'>
       <img src={logo} alt="Logo" />
       <Navbar /> 
     </header>
--- src/components/MainArticle.jsx
+++ src/components/MainArticle.jsx
@@ -3,7 +3,7 @@ import imagenDesktop from "../assets/images/image-web-3-desktop.jpg";
 
 export const MainArticle = () => {
   return (
-    <section>
+    <section className="mb-12">
       <picture>
         <source media="(max-width: 640px)" srcSet={imagenMobile} />
         <source media="(min-width: 641px)" srcSet={imagenDesktop} />
@@ -11,7 +11,9 @@ export const MainArticle = () => {
       </picture>
       <div className="sm:flex">
         <div className="flex-1 py-6">
-          <h2 className="text-[40px] font-bold sm:text-[58px] leading-none ">The Bright Future of Web 3.0?</h2>
+          <h2 className="text-[40px] font-bold sm:text-[58px] leading-none ">
+            The Bright Future of Web 3.0?
+          </h2>
         </div>
         <div className="flex-1 pt-9">
           <p className="text-[13px] mb-10 sm:text-[15px]">
@@ -19,7 +21,9 @@ export const MainArticle = () => {
             power of the platforms back into the hands of the people. But is it
             really fulfilling its promise?
           </p>
-          <button className="bg-SoftRed w-[185px] h-[48px] uppercase text-OffWhite hover:bg-VeryDarkBlue">Read more</button>
+          <button className="bg-SoftRed w-[185px] h-[48px] uppercase text-OffWhite hover:bg-VeryDarkBlue">
+            Read more
+          </button>
         </div>
       </div>
     </section>
