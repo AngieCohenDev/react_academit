46c5db3c8346730413b0b83adc9f5e815762aa3a
Angie Cohen <angiedev2001@gmail.com>
2024-04-24 18:27:34 -0300

    fin de la creacion del componente de video

--- src/pages/VideosPage.jsx
+++ src/pages/VideosPage.jsx
@@ -1,11 +1,15 @@
 import { VideoPlayer } from "../components/VideoPlayer"
+import {Header} from '../components/Header'
 
 import video from '../assets/videos/video_prueba.mp4'
 
 export const VideosPage = () => {
   return (
+    <main className="px-8 pt-10">
+    <Header />
     <div>
         <VideoPlayer src={video}/>
     </div>
+    </main>
   )
 }
