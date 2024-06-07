1277ad133e60ad3c5d07d48b793db5595632ab02
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-07 14:26:39 -0300

    configuracion del componente MenuContainer para recibir la informacion de los props enviada desde el componente VideosPage

--- src/components/MenuContainer.jsx
+++ src/components/MenuContainer.jsx
@@ -1,28 +1,42 @@
 import { MenuItems } from "./MenuItems";
 
-export const MenuContainer = () => {
+export const MenuContainer = ({ onFileVideo, isLoading, cursos }) => {
+
   return (
     <div className="bg-VeryDarkBlue p-4 rounded-lg w-[500px] ">
       <MenuItems
         title="Metodos de JavaScript"
         description="Esta es la descripcion de la clase"
       />
-      <MenuItems
-        title="Clase #2"
-        description="Esta es la descripcion de la clase"
-      />
-      <MenuItems
-        title="Clase #3"
-        description="Esta es la descripcion de la clase"
-      />
-      <MenuItems
-        title="Clase #3"
-        description="Esta es la descripcion de la clase"
-      />
-      <MenuItems
-        title="Clase #3"
-        description="Esta es la descripcion de la clase"
-      />
+      {
+        isLoading
+          ? <>
+            
+            <MenuItems
+              title="Clase #2"
+              description="Esta es la descripcion de la clase"
+            />
+            <MenuItems
+              title="Clase #3"
+              description="Esta es la descripcion de la clase"
+            />
+            <MenuItems
+              title="Clase #3"
+              description="Esta es la descripcion de la clase"
+            />
+            <MenuItems
+              title="Clase #3"
+              description="Esta es la descripcion de la clase"
+            />
+          </>
+          : cursos.map(curso => (
+            <MenuItems 
+              title={curso.title} 
+              description={curso.description} 
+              video={curso.video} 
+              onFileVideo={onFileVideo}/>
+          ))
+      }
     </div>
   );
 };
