161c26305be7a027c2e141597511a7b86dacb571
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-07 14:27:43 -0300

    configuracion de Videos Page enviando props al componte MenuContainer

--- src/pages/VideosPage.jsx
+++ src/pages/VideosPage.jsx
@@ -3,15 +3,30 @@ import {Header} from '../components/Header'
 import { MenuContainer } from "../components/MenuContainer"
 
 import video from '../assets/videos/prueba2.mp4'
+import { useState } from "react"
+import { useFetchCursos } from "../hooks/useFetchCursos"
 
 
 export const VideosPage = () => {
+
+  const [videoUrl, setVideoUrl] = useState(video)
+  const { cursos, isLoading} = useFetchCursos();
+
+  const handleFileVideo = (video) => {
+    setVideoUrl(video)
+  }
+
   return (
     <main className="px-8 pt-10">
     <Header />
     <div className="lg:flex lg:gap-8 ">
-        <VideoPlayer src={video}/>
-        <MenuContainer />
+
+        <VideoPlayer src={videoUrl}/>
+        
+        <MenuContainer 
+          onFileVideo={handleFileVideo} 
+          cursos={cursos} 
+          isLoading={isLoading}/>
     </div>
     </main>
   )
