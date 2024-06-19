bdd613daed4b27c86ab3dbbb221966a981a7a73d
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-19 14:34:03 -0300

    modificado el componente NewContainer.jsx para que su ancho sea equivalente al 30% de su contenedor y se envia la prop key al componente NewArticle.jsx

--- src/components/NewContainer.jsx
+++ src/components/NewContainer.jsx
@@ -5,7 +5,7 @@ export const NewContainer = () => {
 
   const { newArticles, isLoading } = useFetchNewArticles()
   return (
-    <aside className="bg-VeryDarkBlue text-OffWhite py-[28px] px-[20px] ">
+    <aside className="bg-VeryDarkBlue text-OffWhite py-[28px] px-[20px] w-[30%]">
       <h1 className="text-SoftOrange text-4xl font-bold">{newArticles[0]?.sectiontitle}</h1>
       {
         isLoading
@@ -22,7 +22,7 @@ export const NewContainer = () => {
               text={'Amplía tus oportunidades laborales al convertirte en un experto a nivel Front-end y Back-end. ¡Obtén habilidades excepcionales!  '} />
           </>
           : newArticles.map((article) => (
-            <NewArticle title={article.articletitle} text={article.description} />
+            <NewArticle key={article.id} title={article.articletitle} text={article.description} />
           ))
       }
     </aside>
