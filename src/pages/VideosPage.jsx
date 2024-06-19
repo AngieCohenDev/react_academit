78e9cb7523660f25457a41fae3e106de1459151c
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-19 14:38:33 -0300

    Se modifica el componente VideosPage.jsx para que cuando existan videos aparesca inmediatamente

--- src/pages/VideosPage.jsx
+++ src/pages/VideosPage.jsx
@@ -1,31 +1,38 @@
 import { VideoPlayer } from "../components/VideoPlayer"
-import {Header} from '../components/Header'
+import { Header } from '../components/Header'
 import { MenuContainer } from "../components/MenuContainer"
 import video from '../assets/videos/prueba2.mp4'
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useFetchCursos } from "../hooks/useFetchCursos"
 
 export const VideosPage = () => {
 
+  const { cursos, isLoading } = useFetchCursos();
   const [videoUrl, setVideoUrl] = useState(video)
-  const { cursos, isLoading} = useFetchCursos();
+
+  let curso;
+  cursos ? curso = cursos[0]?.video : curso = video
 
   const handleFileVideo = (video) => {
     setVideoUrl(video)
   }
 
+  useEffect(() => {
+    handleFileVideo(curso)
+  }, [cursos])
+
   return (
     <main className="px-8 pt-10">
-    <Header />
-    <div className="lg:flex lg:gap-8 ">
+      <Header />
+      <div className="lg:flex lg:gap-8 ">
 
-        <VideoPlayer src={videoUrl}/>
-        
-        <MenuContainer 
-          onFileVideo={handleFileVideo} 
-          cursos={cursos} 
-          isLoading={isLoading}/>
-    </div>
+        <VideoPlayer src={videoUrl} />
+
+        <MenuContainer
+          onFileVideo={handleFileVideo}
+          cursos={cursos}
+          isLoading={isLoading} />
+      </div>
     </main>
   )
 }
