06d489e85ef2e898fca714253deadfaaaa1da85c
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-19 14:37:42 -0300

    Se limitan los datos de NewArticles a 3

--- src/helpers/getNewArticle.js
+++ src/helpers/getNewArticle.js
@@ -3,10 +3,12 @@ export const getNewArticle = async () => {
     const resp = await fetch( url );
 
     const {data} = await resp.json();
-    const newArticles = data.map( newArticle => ({
-        ...newArticle
-    }))
-    console.log(newArticles);
-    newArticles.reverse()
-    return newArticles;
+    // const newArticles = data.map( newArticle => ({
+    //     ...newArticle
+    // }))
+
+    console.log(data);
+    data.reverse()
+    const limitedData = data.slice(0, 3);  // Limitar la cantidad de datos a 3
+    return limitedData;
 }
