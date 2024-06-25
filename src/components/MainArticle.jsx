6ad0f808afdfeb4d6a4c42af592055b7b47643e5
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-25 16:27:48 -0300

    Se modifica el componente MainArticle.jsx y NavBar.jsx para manejar informacion de mainArticle y los links de navegacion en cada item del header

--- src/components/MainArticle.jsx
+++ src/components/MainArticle.jsx
@@ -3,12 +3,14 @@ import imagenDesktop from "../assets/images/foto.jpg";
 import { useFetchMainArticles } from "../hooks/useFetchMainArticle";
 
 export const MainArticle = () => {
+
   const { mainArticle, isLoading } = useFetchMainArticles()
+
   return (
     <section className="mb-12 w-[70%]">
       <picture>
-        <source media="(max-width: 640px)" srcSet={isLoading ? imagenMobile : mainArticle[0]?.imageUrl} />
-        <source media="(min-width: 641px)" srcSet={isLoading ? imagenDesktop : mainArticle[0]?.imageUrl} />
+        <source media="(max-width: 640px)" srcSet={isLoading ? imagenMobile : mainArticle?.imageUrl} />
+        <source media="(min-width: 641px)" srcSet={isLoading ? imagenDesktop : mainArticle?.imageUrl} />
         <img
           src={isLoading ? imagenDesktop : mainArticle[0]?.imageUrl}
           alt="Articulo principal imagen" style={{ display: 'inline-block', width: '1460px', height: '350px' }} />
@@ -19,7 +21,7 @@ export const MainArticle = () => {
             {
               (isLoading)
                 ? '¡Aprende de programación con expertos!'
-                : (mainArticle[0]?.title.length !== 0 ? mainArticle[0].title : '¡Aprende de programación con expertos!')
+                : (mainArticle?.title.length !== 0 ? mainArticle.title : '¡Aprende de programación con expertos!')
             }
           </h2>
         </div>
@@ -28,14 +30,14 @@ export const MainArticle = () => {
             {
               isLoading
                 ? 'Nos comprometemos a fomentar el crecimiento y la excelencia de aquellos que confían en nosotros, contribuyendo así al éxito de nuestros estudiantes.'
-                : (mainArticle[0]?.description.length !== 0 ? mainArticle[0].description : 'Nos comprometemos a fomentar el crecimiento y la excelencia de aquellos que confían en nosotros, contribuyendo así al éxito de nuestros estudiantes.')
+                : (mainArticle?.description.length !== 0 ? mainArticle.description : 'Nos comprometemos a fomentar el crecimiento y la excelencia de aquellos que confían en nosotros, contribuyendo así al éxito de nuestros estudiantes.')
             }
           </p>
           <button className="bg-SoftRed w-[185px] h-[48px] uppercase text-OffWhite hover:bg-VeryDarkBlue">
             {
               isLoading
                 ? 'Inscribirse'
-                : (mainArticle[0]?.textButton.length !== 0 ? mainArticle[0]?.textButton : 'Inscribirse')
+                : (mainArticle?.textButton.length !== 0 ? mainArticle?.textButton : 'Inscribirse')
             }
           </button>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,16 @@ export const Navbar = ({items}) => {
     <>
     <ul className="hidden sm:flex text-[18px] sm:w-[538px] sm:place-content-around sm:text-[16px] sm:items-center">
       <li >
-        <a href="/">{items.item01 ? items.item01 : 'Inicio'}</a>
+        <a href={items?.NavegacionItem01}>{items.item01 ? items.item01 : 'Inicio'}</a>
       </li>
       <li>
-        <a href='video'>{items.item02 ? items.item02 : 'Nuestros programas'}</a>
+        <a href={items?.NavegacionItem02}>{items.item02 ? items.item02 : 'Nuestros programas'}</a>
       </li>
       <li>
-        <a href="#">{items.item03 ? items.item03 : 'Sobre nosotros'}</a>
+        <a href={items?.NavegacionItem03}>{items.item03 ? items.item03 : 'Sobre nosotros'}</a>
       </li>
       <li>
-        <a href="#">{items.item04 ? items.item04 : 'Características'}</a>
+        <a href={items?.NavegacionItem04}>{items.item04 ? items.item04 : 'Características'}</a>
       </li>
     </ul>
     <img className='w-10 h-4 cursor-pointer sm:hidden' src={burgerMenu} alt="Menu hamburguesa" />
