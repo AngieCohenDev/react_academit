6a25559df0bcb097154c2b97521c6b005f8a156c
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-26 16:21:58 -0300

    Se crea pantalla de informacion de academit

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,13 @@
+import { Header } from '../components/Header'
+
+export const AboutPage = () => {
+    return (
+        <>
+            <Header />
+            <div>
+                <p>Pagina para Hablar sobre academit</p>
+            </div>
+        </>
+
+    )
+}
