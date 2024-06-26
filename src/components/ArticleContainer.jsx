c561f1387c4cbe99e390f6804bef2ec024538530
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-26 12:07:35 -0300

    se envia al componente Article.jsx la prop link para que reciba la ruta a la cual navegar

--- src/components/ArticleContainer.jsx
+++ src/components/ArticleContainer.jsx
@@ -1,8 +1,8 @@
 import img1 from "../assets/images/inicio_foto_img.jpg";
 import img2 from "../assets/images/img2.jpg";
 import img3 from "../assets/images/img3.jpg";
-
 import { Article } from "./Article";
+
 import { useFetchArticles } from "../hooks/useFetchArticles";
 
 export const ArticleContainer = () => {
@@ -16,15 +16,18 @@ export const ArticleContainer = () => {
             <Article
               title={'Enseñanza totalmente en línea'}
               text={'Contamos con horarios flexibles para que se adapten a la disponibilidad del estudiante.'}
-              img={img1} />
+              img={img1}
+              link={'/'} />
             <Article
               title={'Asistencia individualizada'}
               text={'Nuestros profesionales están para resolver todas las dudas e inquietudes que surjan en tu camino educativo.'}
-              img={img2} />
+              img={img2} 
+              link={'/'} />
             <Article
               title={'Apoyo financiero'}
               text={'Tenemos a tu disposición diversas opciones para que puedas financiar tu educación con nosotros.'}
-              img={img3} />
+              img={img3} 
+              link={'/'} />
           </>
           : articles.map((article) => (
             <Article
@@ -32,6 +35,7 @@ export const ArticleContainer = () => {
               title={article.title}
               text={article.description}
               img={article.imageUrl}
+              link={article.NavegacionTitle}
             />
           ))
       }
