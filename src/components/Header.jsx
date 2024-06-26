607911e765c78660269a922484e239c1b4d46d8d
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-06-26 12:08:25 -0300

    add: se agrega ruta de navegacion a la imagen del logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,14 +6,13 @@ import { useFetchItems } from '../hooks/useFetchItems';
 export const Header = () => {
 
   const { items, isLoading } = useFetchItems()
-  
-  console.log(items);
+
   return (
     <header className=' flex place-content-between items-center mb-8'>
       {
         isLoading
-        ? <img src={logo} alt="Logo" style={{display: 'inline-block', width: '100px'}} />
-        : <img src={items.logo} alt="Logo" style={{display: 'inline-block', width: '100px'}} />
+        ? <a href="/" ><img src={logo} alt="Logo" style={{display: 'inline-block', width: '100px', cursor: 'pointer'}} /></a>
+        : <a href="/" ><img src={items.logo} alt="Logo" style={{display: 'inline-block', width: '100px', cursor: 'pointer'}} /></a>
        
       }
       <Navbar items={items} />
