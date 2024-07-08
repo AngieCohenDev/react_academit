b61f79d098a51c8bbd2ddf80feb90396ed26cc88
Angie Cohen <angiedev2001@gmail.com>
2024-07-08 12:01:09 -0300

    add: se creo el componente Card para crear las tarjetas de vista previa de los cursos

--- src/components/Curso.jsx
+++ src/components/Curso.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const Curso = ({ title, text, image, link }) => {
     return (
         <a href={link}>
-            <div className='overflow-hidden' style={{borderRadius: '5px', backgroundColor: '#F7F7F7', }}>
+            <div className='overflow-hidden border-4 bg-slate-200'>
                 <img src={image} alt=""/>
                 <h1 className='font-sans text-xl bold' style={{paddingTop: '5px', paddingBottom: '2px'}}>{title}</h1>
                 <p className='font-sans' style={{paddingTop: '2px', paddingBottom: '2px'}}>{text}</p>
--- src/pages/CursosPage.jsx
+++ src/pages/CursosPage.jsx
@@ -1,35 +1,64 @@
 import { Header } from "../components/Header";
-import { Curso } from "../components/Curso";
+import { Card } from "../components/Card";
 
 import img1 from "../assets/images/js.png";
 import img2 from "../assets/images/gitygithub.png";
 import img3 from "../assets/images/csharp.png";
 
 export const CursosPage = () => {
-
     return (
         <main className="px-8 pt-10">
             <Header />
             <div className="mt-10 grid gap-10 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
-                <Curso
+                <Card
                     key={1}
                     title={'JavaScript de 0 a Experto'}
                     text={'Curso de JavaScript desde las bases hasta ser un experto.'}
                     image={img1}
-                    link={'/video'} />  
-                <Curso
+                    link={'/video'}
+                    tags={['#JavaScript', '#Programación', '#Desarrollo']} 
+                />
+                <Card
                     key={2}
                     title={'Git y GitHub para principiantes'}
                     text={'Curso de Git y GitHub desde las bases.'}
                     image={img2}
-                    link={'#'} />
-                <Curso
+                    link={'#'}
+                    tags={['#Git', '#GitHub', '#Control de versiones']} 
+                />
+                <Card
                     key={3}
                     title={'C# Curso de 0 a Experto'}
                     text={'Curso de C# desde las bases hasta ser un experto.'}
                     image={img3}
-                    link={'#'} />
+                    link={'#'}
+                    tags={['#C#', '#Programación', '#Desarrollo']} 
+                />
+                <Card
+                    key={4}
+                    title={'JavaScript de 0 a Experto'}
+                    text={'Curso de JavaScript desde las bases hasta ser un experto.'}
+                    image={img1}
+                    link={'/video'}
+                    tags={['#JavaScript', '#Frontend', '#Web']} 
+                />
+                <Card
+                    key={5}
+                    title={'Git y GitHub para principiantes'}
+                    text={'Curso de Git y GitHub desde las bases.'}
+                    image={img2}
+                    link={'#'}
+                    tags={['#Git', '#GitHub', '#Colaboración']} 
+                />
+                <Card
+                    key={6}
+                    title={'C# Curso de 0 a Experto'}
+                    text={'Curso de C# desde las bases hasta ser un experto.'}
+                    image={img3}
+                    link={'#'}
+                    tags={['#C#', '#Backend', '#Desarrollo']} 
+                />
             </div>
         </main>
-    )
+    );
 }
