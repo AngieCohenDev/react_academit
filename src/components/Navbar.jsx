24f3c8d3b90ad1146227652ac501be9decd29c75
Frank Jose Jimenez Pacheco <frankjimenezp113@gmail.com>
2024-07-10 20:57:29 -0300

    [checkpoint]: modificado header para que tenga rutas predefinidas en caso de no tener datos en los campos de navegacionitem de la base de datos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,16 +6,16 @@ export const Navbar = ({items}) => {
     <>
     <ul className="hidden sm:flex text-[18px] sm:w-[538px] sm:place-content-around sm:text-[16px] sm:items-center">
       <li >
-        <a href={items?.NavegacionItem01}>{items.item01 ? items.item01 : 'Inicio'}</a>
+        <a href={items?.NavegacionItem01 ? items?.NavegacionItem01 : '/'}>{items.item01 ? items.item01 : 'Inicio'}</a>
       </li>
       <li>
-        <a href={items?.NavegacionItem02}>{items.item02 ? items.item02 : 'Nuestros programas'}</a>
+        <a href={items?.NavegacionItem02 ? items?.NavegacionItem02 : '/cursos' }>{items.item02 ? items.item02 : 'Nuestros programas'}</a>
       </li>
       <li>
-        <a href={items?.NavegacionItem03}>{items.item03 ? items.item03 : 'Sobre nosotros'}</a>
+        <a href={items?.NavegacionItem03 ? items?.NavegacionItem03 : '/informacion' }>{items.item03 ? items.item03 : 'Sobre nosotros'}</a>
       </li>
       <li>
-        <a href={items?.NavegacionItem04}>{items.item04 ? items.item04 : 'Contacto'}</a>
+        <a href={items?.NavegacionItem04 ? items?.NavegacionItem02 : '/contacto'}>{items.item04 ? items.item04 : 'Contacto'}</a>
       </li>
     </ul>
     <img className='w-10 h-4 cursor-pointer sm:hidden' src={burgerMenu} alt="Menu hamburguesa" />
